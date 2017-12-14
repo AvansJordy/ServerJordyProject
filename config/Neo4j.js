443e259586fa4b086a147fcb1c37222ddb07e20f
Jcjmfrij <jcjmfrij@avans.nl>
2017-12-14 14:02:07 +0100

    Neo4j fix

--- config/Neo4j.js
+++ config/Neo4j.js
@@ -4,7 +4,7 @@
 const neo4j = require('neo4j-driver').v1;
 
 if(process.env.NODE_ENV ===  'production'){
-    var driver = neo4j.driver("bolt://hobby-mkdmfblfobflgbkenjomhjal.dbs.graphenedb.com:24786", neo4j.auth.basic("user", "b.0qSQcAG0a2tG.ELrDP8XCo0P9ga4s"));
+    var driver = neo4j.driver("bolt://hobby-cfknfblfobflgbkeneknhjal.dbs.graphenedb.com:24786", neo4j.auth.basic("user", "b.0qSQcAG0a2tG.ELrDP8XCo0P9ga4s"));
 }else {
     var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("", ""));
 }
